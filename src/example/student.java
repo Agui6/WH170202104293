b5b0bc517fd1ebf31fab2f6aa139ef52542c1bfa
Administrator <Administrator@MD501-88>
2019-05-24 12:44:28 +0800

    作业

---
+++ src/example/student.java
@@ -0,0 +1,15 @@
+package example;
+
+public class student {
+	int age;
+	String name;
+	String ID;
+	student(){
+		
+	}
+	student(int age,String name,String ID){
+	this.age=age;
+	this.name=name;
+	this.ID=ID;
+	}
+}
---
+++ src/example/teacher.java
@@ -0,0 +1,16 @@
+package example;
+
+public class teacher {
+	int age;
+	String name;
+	String ID;
+	teacher(){
+		
+	}
+	teacher(int age,String name,String ID){
+	this.age=age;
+	this.name=name;
+	this.ID=ID;
+	}
+}
+
